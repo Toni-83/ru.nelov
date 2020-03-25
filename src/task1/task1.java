5506d856025af863d5182407b5e01a6718a0d830
Toni-83 <crazy_mane@mail.ru>
2020-03-26 02:49:57 +0300

    Добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел.

--- src/task1/task1.java
+++ src/task1/task1.java
@@ -8,14 +8,47 @@ public class task1 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a,b;
-        System.out.print("введите первое число: ");
-        a = in.nextInt();
-        System.out.print("введите второе число: ");
-        b = in.nextInt();
-        System.out.println("первое число = "+a);
-        System.out.println("второе число = "+b);
-        b=a-b;
-        System.out.println("разница" + b);
+        double a,b;
+        int i=0;
+        do {
+            System.out.print("Введите первое целое число: ");
+            a = in.nextDouble();
+            i++;
+            if ((a-(int)a) != 0)  {
+                if (i<5){
+                    System.out.println("Введено дробное число, введите число еще раз. У вас осталось " + (5-i) + " попыток.");
+                } else {
+                        System.out.println("Вы исчерпали количество попыток");
+                        System.exit(0);
+                }
+            }
+        } while ((a-(int)a) != 0);
+        i=0;
+        do {
+            System.out.print("Введите первое целое число: ");
+            b = in.nextDouble();
+            i++;
+            if ((b-(int)b) != 0)  {
+                if (i<5) {
+                    System.out.println("Введено дробное число, введите число еще раз. У вас осталось " + (5 - i) + " попыток.");
+                } else {
+                        System.out.println("Вы исчерпали количество попыток");
+                        System.exit(0);
+                }
+            }
+        } while ((b-(int)b) != 0);
+        if (a<b) {
+            System.out.println("Минимальное число = "+(int)a);
+        }
+        else {
+            if (a==b) {
+                System.out.println("Введены равные числа и равны значению "+(int)b);
+            }
+            else {
+                System.out.println("Минимальное число = "+(int)b);
+            }
+        }
+        System.out.println("первое число = "+(int)a);
+        System.out.println("второе число = "+(int)b);
     }
 }
