10f029d848bbf5d8d52b20c90a95bd82ae2ce41b
Toni-83 <crazy_mane@mail.ru>
2020-03-27 01:52:09 +0300

    в первом задании добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел. Выполнено первое и второе задание. изменена структура, разбито по ДЗ. переименование Дополнено заданием 3: Ввывод таблицы умножения для вводимого числа. Дополнено четвертой задачей.

--- src/homework4/task4.java
+++ src/homework4/task4.java
@@ -5,10 +5,45 @@ import java.util.Scanner;
 public class task4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double n, rpog = 0;
+        double n, prog;
+        int i = 0;
         do {
-        System.out.println(" ");
-        n = in.nextDouble();
-        } while ((n-(int)n) != 0 | rpog<1 | rpog>2);
+            System.out.print("Введите положительное целое число N: ");
+            n = in.nextDouble();
+            i++;
+            if ((n-(int)n) != 0 | n<0)  {
+                if (i<5){
+                    System.out.println("Введено дробное число, введите число еще раз. У вас осталось " + (5-i) + " попыток.");
+                } else {
+                    System.out.println("Вы исчерпали количество попыток");
+                    System.exit(0);
+                }
+            }
+        } while ((n-(int)n) != 0 | n<0);
+        i=0;
+        do {
+            System.out.print("Введите число 1 если требуется расчитать арифметическую прогрессию\nчисло 2 - если геометрическую прогрессию: ");
+            prog = in.nextDouble();
+            i++;
+            if ((prog-(int)prog) != 0 | prog < 1 | prog > 2)  {
+                if (i<5) {
+                    System.out.println("Введено неверное значение, введите значение еще раз. У вас осталось " + (5 - i) + " попыток.");
+                } else {
+                    System.out.println("Вы исчерпали количество попыток");
+                    System.exit(0);
+                }
+            }
+        } while ((prog-(int)prog) != 0 | prog < 1 | prog > 2);
+        if (prog == 1) {
+            System.out.println("Выведена арифметическая прогрессия числа " + (int)n+":");
+            for (int a=0;a<=n;a++) {
+                System.out.println((int)n+a);
+            }
+        } else {
+            System.out.println("Выведена геометрическая прогрессия числа " + (int)n+":");
+            for (int a=1;a<=n;a++) {
+                System.out.println((int)n*a);
+            }
+        }
     }
 }
