b30993562b8d6567a3baac3c3fdc63df88124408
Toni-83 <crazy_mane@mail.ru>
2020-03-27 01:01:06 +0300

    в первом задании добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел. Выполнено первое и второе задание. изменена структура, разбито по ДЗ. переименование Дополнено заданием 3: Ввывод таблицы умножения для вводимого числа. Начата четвертая задача.

--- src/homework4/task4.java
+++ src/homework4/task4.java
@@ -1,7 +1,14 @@
 package homework4;
 
+import java.util.Scanner;
+
 public class task4 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        double n, rpog = 0;
+        do {
+        System.out.println(" ");
+        n = in.nextDouble();
+        } while ((n-(int)n) != 0 | rpog<1 | rpog>2);
     }
 }
