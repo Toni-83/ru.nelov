21f609066626925e09e4112d8f0b1046201edf74
Toni-83 <crazy_mane@mail.ru>
2020-03-26 22:29:56 +0300

    в первом задании добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел. Выполнено первое и второе задание. изменена структура, разбито по ДЗ. переименование Дополнено заданием 3: Ввывод таблицы умножения.

--- src/homework4/task3.java
+++ src/homework4/task3.java
@@ -2,7 +2,11 @@ package homework4;
 
 public class task3 {
     public static void main(String[] args) {
-
-
+        for (int a=1; a<=10;a++) {
+            System.out.println("\n Умножение числа "+a);
+            for (int b=1; b<=10;b++){
+                System.out.println(a+"*"+b+"="+(a*b));
+            }
+        }
     }
 }
--- src/homework4/task4.java
+++ src/homework4/task4.java
@@ -2,6 +2,6 @@ package homework4;
 
 public class task4 {
     public static void main(String[] args) {
-        // write your code here
+
     }
 }
