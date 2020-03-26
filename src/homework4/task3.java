00b95c4d8a7f84fe9558102713c24cbdc76db152
Toni-83 <crazy_mane@mail.ru>
2020-03-27 00:13:37 +0300

    в первом задании добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел. Выполнено первое и второе задание. изменена структура, разбито по ДЗ. переименование Дополнено заданием 3: Ввывод таблицы умножения для вводимого числа.

--- src/homework4/task3.java
+++ src/homework4/task3.java
@@ -1,12 +1,29 @@
 package homework4;
 
+import java.util.Scanner;
+
 public class task3 {
     public static void main(String[] args) {
-        for (int a=1; a<=10;a++) {
-            System.out.println("\n Умножение числа "+a);
-            for (int b=1; b<=10;b++){
-                System.out.println(a+"*"+b+"="+(a*b));
+        Scanner in = new Scanner(System.in);
+        double a;
+        System.out.print("В ведите число от 1 до 10 для которого требуется вывести таблицу умножения. \n Число: ");
+        int i = 0;
+        do {
+            a = in.nextDouble();
+            i++;
+            if (a<1 | (a-(int)a) != 0 | a>10) {
+            if (i<5) {
+                System.out.print("Введено не верное число, осуществите ввод еще раз. \n Число: ");
             }
-        }
+            else {
+                System.out.println("Вы привысили количество попыток, ввели ошибочное значение более 5 раз.");
+                System.exit(0);
+            }
+            }
+        } while ((a-(int)a) != 0 | a<1 | a>10);
+            System.out.println("\n Умножение числа "+(int)a);
+            for (int b=1; b<=10; b++){
+                System.out.println((int)a+"*"+b+"="+(int)(a*b));
+            }
     }
 }
