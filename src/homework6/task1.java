26d6d907a7f1d9833935793ef499ea7b127b8738
Toni-83 <crazy_mane@mail.ru>
2020-04-04 01:07:22 +0300

    в первом задании добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел. Выполнено первое и второе задание. изменена структура, разбито по ДЗ. переименование Дополнено заданием 3: Ввывод таблицы умножения для вводимого числа. Дополнено четвертой задачей. Добавлены файлы для второго и третьего ДЗ.

---
+++ src/homework6/task1.java
@@ -0,0 +1,29 @@
+package homework6;
+public class task1 {
+
+    public class humane {
+    public int age;
+    enum sex {
+        man,
+        woman
+    }
+    public void live() {
+        age = 0;
+        System.out.println("Humane is alive!");
+    }
+        }
+        class student extends humane {
+        humane h = new humane();
+        int cours;
+        public void student() {
+            h.live(); {
+                System.out.println("Student is studying in ");
+        }
+    }
+
+}
+
+    public static void main(String[] args) {
+
+    }
+}
