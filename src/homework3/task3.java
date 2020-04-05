2da76ee153971e103cef13d3691a188b73b93e2c
Toni-83 <crazy_mane@mail.ru>
2020-04-05 05:22:29 +0300

    в первом задании добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел. Выполнено первое и второе задание. изменена структура, разбито по ДЗ. переименование Дополнено заданием 3: Ввывод таблицы умножения для вводимого числа. Дополнено четвертой задачей. Добавлены файлы для второго и третьего ДЗ. Сделана Вендинговая машина. ДЗ 6 наследование.

--- src/homework3/task3.java
+++ src/homework3/task3.java
@@ -1,7 +1,62 @@
 package homework3;
 
+import java.util.Scanner;
+
+class Napitok {
+    String name;
+    int price;
+
+    public Napitok (String inputname, int inputprice){
+        this.name=inputname;
+        this.price=inputprice;
+    }
+}
+class PriceList {
+    Napitok[] napitoks = new Napitok[10];
+    public void printPriceList (){
+        napitoks[0]= new Napitok("Kolla", 2);
+        napitoks[1]= new Napitok("Coffe", 3);
+        for (int i=0; i<napitoks.length;i++) {
+            if (napitoks[i]!= null){
+                System.out.println((i+1)+". "+napitoks[i].name+ " price: "+napitoks[i].price);
+            }
+        }
+    }
+}
+class Choice {
+    int cash;
+    int choice;
+    Scanner in = new Scanner(System.in);
+    public void choice(){
+        System.out.println("Enter amount: ");
+        cash = in.nextInt();
+        System.out.println("Enter a number of a drink: ");
+        choice = in.nextInt();
+    }
+}
+class Verification {
+    PriceList priceList = new PriceList();
+    Choice choice = new Choice();
+    int summ = 0;
+    public void verification (){
+        priceList.printPriceList();
+        choice.choice();
+        summ = choice.cash;
+        while (choice.choice != 0 && summ != priceList.napitoks[choice.choice-1].price) {
+            System.out.println("clr");
+            priceList.printPriceList();
+            System.out.println("Not enough money, you need to replenish for "
+                    +(priceList.napitoks[choice.choice -1].price - summ) + " RUB." );
+            choice.choice();
+            summ = summ + choice.cash;
+        }
+        System.out.println("Thank you for your purchase. Pick up change.");
+    }
+}
 public class task3 {
     public static void main(String[] args){
+        Verification verification = new Verification();
+        verification.verification();
 
     }
 }
--- src/homework6/task1.java
+++ src/homework6/task1.java
@@ -1,29 +1,69 @@
 package homework6;
+
+
 public class task1 {
 
-    public class humane {
-    public int age;
-    enum sex {
+    public static class Human {
+        public int age;
+        public Sex sex;
+        String name;
+
+        public Human(String name, Sex sex, int age) {
+            this.age = age;
+            this.sex = sex;
+            this.name = name;
+        }
+
+        public void DuActiv() {
+            System.out.println("A "+ name + " sam Axctiv!");
+        }
+    }
+
+    enum Sex {
         man,
         woman
     }
-    public void live() {
-        age = 0;
-        System.out.println("Humane is alive!");
-    }
-        }
-        class student extends humane {
-        humane h = new humane();
-        int cours;
-        public void student() {
-            h.live(); {
-                System.out.println("Student is studying in ");
-        }
-    }
 
-}
+    static class Student extends Human {
+        int cours;
+        int group;
+
+        public Student(String name, Sex sex, int age, int cours, int group) {
+            super(name, sex, age);
+            this.cours = cours;
+            this.group = group;
+        }
+
+        @Override
+        public void DuActiv() {
+            System.out.println("Student name: " + name +
+                    ", sex: " + sex +
+                    ", age: "+ age +
+                    ", is studying in group: " + group +
+                    " on cours: " + cours + ".");
+        }
+    }
+    static class UniversityStudents{
+        Student[][][] students = new Student[4][5][10];
+        public void printUniversityStudents() {
+            for (int i = 0; i < students.length; i++) {
+                for (int ii = 0; ii < students[i].length; ii++) {
+                    for (int iii = 0; iii < students[i][ii].length; iii++) {
+                        if (students[i][ii][iii] != null) {
+                            students[i][ii][iii].DuActiv();
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
+       UniversityStudents universitystudents = new UniversityStudents();
+       universitystudents.students[1][1][1] = new Student("Toni", Sex.man, 18, 1, 1);
+       universitystudents.students[2][1][1] = new Student("Katty", Sex.woman, 28, 2, 1);
+       universitystudents.printUniversityStudents();
 
-    }
+       }
+
 }
