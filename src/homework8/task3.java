317a0fce657f04b037958d50cf09562c75044fb7
Toni-83 <crazy_mane@mail.ru>
2020-04-09 23:10:47 +0300

    в первом задании добавлена проверка ввода целого числа, а также добавлен функционал на 5 попыток ввода каждого из чисел. Выполнено первое и второе задание. изменена структура, разбито по ДЗ. переименование Дополнено заданием 3: Ввывод таблицы умножения для вводимого числа. Дополнено четвертой задачей. Добавлены файлы для второго и третьего ДЗ. Сделана Вендинговая машина. Доработана. ДЗ 6 наследование. ДЗ 7 второе задание. ДЗ 7 третье задание.

---
+++ src/homework8/task3.java
@@ -0,0 +1,30 @@
+package homework8;
+
+// Подсчет запущенных экземпляров класса.
+class MyCalc {
+    private static int calc;
+    public MyCalc () {
+        calc++;
+    }
+    public int getCalc() {
+        return calc;
+    }
+}
+
+public class task3 {
+    public static void main(String[] args){
+        MyCalc myCalc1 = new MyCalc();
+        MyCalc myCalc2 = new MyCalc();
+        MyCalc myCalc3 = new MyCalc();
+        MyCalc myCalc4 = new MyCalc();
+     System.out.println(new MyCalc().getCalc());
+     System.out.println(new MyCalc().getCalc());
+     System.out.println(new MyCalc().getCalc());
+     System.out.println(new MyCalc().getCalc());
+     System.out.println("------------------------------------------------------------------------");
+        System.out.println(myCalc1.getCalc());
+        System.out.println(myCalc2.getCalc());
+        System.out.println(myCalc3.getCalc());
+        System.out.println(myCalc4.getCalc());
+    }
+}
