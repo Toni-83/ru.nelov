27032569f0c67f2cc653a5cc62276e39a8ccd161
Toni-83 <crazy_mane@mail.ru>
2020-04-11 19:49:36 +0300

    ДЗ 7 исправлены баги и максимально уменьшина связанность

--- src/homework7/task1.java
+++ src/homework7/task1.java
@@ -13,7 +13,7 @@ class Napitok {
 }
 class PriceList {
     Napitok[] napitoks = new Napitok[10];
-    public void printPriceList (){
+    public void PriceList(){
         napitoks[0]= new Napitok("Kolla", 2);
         napitoks[1]= new Napitok("Coffe", 3);
 
@@ -25,45 +25,55 @@ class PriceList {
     }
 }
 class Choice {
-    PriceList priceList = new PriceList();
     int cash;
     int choice;
     Scanner in = new Scanner(System.in);
-    public void choice(){
-        priceList.printPriceList();
+    public void Choice(PriceList priceList) {
+        Choice ch;
         System.out.println("Enter amount: ");
         cash = in.nextInt();
         System.out.println("Enter a number of a drink, ore 0 to exit: ");
         choice = in.nextInt();
-        while (( choice <= 0 || 10 <= choice) || priceList.napitoks[choice-1] == null){
+        while ((choice < 0 || choice >= 10) || (choice != 0 && priceList.napitoks[choice - 1] == null)) {
             System.out.println("You enter wrong number of a drink.");
-            priceList.printPriceList();
-            System.out.println ("Please choice again, ore enter 0 to exit: ");
-            choice=in.nextInt();
+            priceList.PriceList();
+            System.out.println("Please choice again, ore enter 0 to exit: ");
+            choice = in.nextInt();
         }
     }
 }
+
 class Verification {
-    Choice choice = new Choice();
     int summ = 0;
     int price = 0;
-    public void verification (){
-        choice.choice();
+    public void verification (PriceList priceList, Choice choice) {
+        if (choice.choice == 0) {
+            System.out.println("Thank you for your purchase. Pick up change: " + (choice.cash) + " .");
+            System.exit(0);
+        }
         summ = choice.cash;
-        while (choice.choice != 0 && summ < choice.priceList.napitoks[choice.choice-1].price) {
-            price = choice.priceList.napitoks[choice.choice - 1].price;
-            System.out.println("Not enough money, you need to replenish for "+ (price - summ) + " RUB.");
-                choice.choice();
-                summ = summ + choice.cash;
+        price = priceList.napitoks[choice.choice - 1].price;
+        while (summ < price) {
+            System.out.println("Not enough money, you need to replenish for " + (price - summ) + " RUB.");
+            choice.Choice(priceList);
+            summ = summ + choice.cash;
+            if (choice.choice == 0) {
+                System.out.println("Thank you for your purchase. Pick up change: " + (summ) + " .");
+                System.exit(0);
+            }
 
         }
-        System.out.println("Thank you for your purchase. Pick up change: "+ (summ-price) +" .");
+        System.out.println("Thank you for your purchase. Pick up change: " + (summ - price) + " .");
     }
 }
 public class task1 {
-    public static void main(String[] args) {
-        Verification verification = new Verification();
-        verification.verification();
 
+    public static void main(String[] args) {
+        PriceList priceList = new PriceList();
+        Choice choice = new Choice();
+        priceList.PriceList();
+        choice.Choice(priceList);
+        Verification verification= new Verification();
+        verification.verification(priceList,choice);
     }
 }
