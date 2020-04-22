dbd3d2cd5362a21da0006a60930184933322906d
Toni-83 <crazy_mane@mail.ru>
2020-04-22 22:24:35 +0300

    ДЗ 15. Сделано полностью. Добавлен подсчет выведенных объектов и добавлена функция вывода полного дерева каталогов.

--- src/homework15/task1.java
+++ src/homework15/task1.java
@@ -4,43 +4,42 @@ import java.io.File;
 import java.util.Scanner;
 
 class Dir {
-    int c =0;
-    public void listDir(String way, int l) throws NullPointerException{
+    public void listDir(String way, int l, int c) throws NullPointerException{
         if (l>-1){
             File[] list;
             list = new File(way).listFiles();
             try {
                 for (int i=0;i<list.length;i++){
-                    PrintDir.printDir(list[i].toString().substring(list[i].toString().lastIndexOf("\\")),c);
+                    PrintNam.printNam(list[i].toString().substring(list[i].toString().lastIndexOf("\\")),c);
+                    task1.calc+=1;
                     if (list[i].isDirectory()) {
-                    listDir(list[i].toString(),(l-1),1);
+                    listDir(list[i].toString(),(l-1), c+1);
                     }
                 }
                 } catch (NullPointerException e){
-                    PrintDir.printDir("Объект не найден.",c);
+                    PrintNam.printNam("Объект является ссылкой.",c);
                 }
         }
     }
-    private void listDir(String way, int l, int c) throws NullPointerException{
-        if (l>-1){
+   public void listDir(String way, int c) throws NullPointerException{
             File[] list;
             list = new File(way).listFiles();
             try {
                 for (int i=0;i<list.length;i++){
-                    PrintDir.printDir(list[i].toString().substring(list[i].toString().lastIndexOf("\\")),c);
+                    PrintNam.printNam(list[i].toString().substring(list[i].toString().lastIndexOf("\\")),c);
+                    task1.calc+=1;
                     if (list[i].isDirectory()) {
-                        listDir(list[i].toString(),(l-1),c+1);
+                        listDir(list[i].toString(),c+1);
                     }
                 }
             } catch (NullPointerException e){
-                PrintDir.printDir("Объект является ссылкой.",c);
-            }
+                PrintNam.printNam("Объект является ссылкой.",c);
             }
         }
     }
 
-class PrintDir{
-    public static void printDir(String list, int c){
+class PrintNam {
+    public static void printNam(String list, int c){
         for (int ii=0; ii<c;ii++){
             System.out.print(" ");
         }
@@ -48,13 +47,26 @@ class PrintDir{
     }
 }
 
+
 public class task1 {
+    public static int calc;
     public static void main(String[] args) {
         Dir dir = new Dir();
         System.out.print("Введите путь к каталогу, обязательно в конце строки долженбыть \"\\\": ");
         String in = new Scanner(System.in).nextLine();
-        System.out.print("Введите глубену вложенности: ");
+        System.out.print("Введите глубену вложенности,\nпри вводе -1 выводится все дерево каталогов с максимальной вложенностью: ");
         int l = new Scanner(System.in).nextInt();
-        dir.listDir(in,l);
+        if(new File(in).listFiles() != null){
+            if (l!=-1){
+                dir.listDir(in,l,0);
+                System.out.println("\n-------Всего выведено "+calc+" объектов.--------");
+            }else {
+                System.out.println("--------------- На будет осуществлен вывод всего дерева каталога "+in+". -------------------");
+                dir.listDir(in,0);
+                System.out.println("\n-------Всего выведено "+calc+" объектов.--------");
+            }
+        }else {
+            PrintNam.printNam("Объект не найден.",0);
+        }
     }
 }
