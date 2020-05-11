fa6efa1ce0cbe7617b4880f6e610de8e11d0e0fe
Toni-83 <crazy_mane@mail.ru>
2020-05-11 03:26:35 +0300

    21. готов.

--- src/homework21/task2.java
+++ src/homework21/task2.java
@@ -1,4 +1,19 @@
 package homework21;
 
+import java.util.Arrays;
+
 public class task2 {
+    static int[] data = {1,2,3,4,5,6,7,8,9};
+    public static void main(String[] args){
+        System.out.print("Исходный массив: ");
+        System.out.println(Arrays.toString(data).replace("[","").replace("]","")+".");
+        System.out.println("");
+        for (int i=0; i<(data.length/2);i++) {
+            int a = data[i];
+            data[i]=data[data.length-(i+1)];
+            data[data.length-(i+1)] = a;
+        }
+        System.out.print("Измененный массив: ");
+        System.out.println(Arrays.toString(data).replace("[","").replace("]","")+".");
+    }
 }
