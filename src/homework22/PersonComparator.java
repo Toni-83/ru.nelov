7fee7e6d8b24e64c4a812d36ed14a2a0facbf232
Toni <crazy_mane@mail.ru>
2020-05-18 01:35:07 +0300

    ДЗ 22 сделано. Окончательный результат.

--- src/homework22/PersonComparator.java
+++ src/homework22/PersonComparator.java
@@ -1,11 +1,53 @@
 package homework22;
 
-class Person{
+import java.util.TreeSet;
+
+class Person implements Comparable<Object> {
     String name;
     int eag;
-    public Person(String name, int eag){
-        this.name=name;
-        this.eag=eag;
+    String TEMPLATE = "name=%s, eag=%d";
+
+    Person(String name, int eag) {
+        this.name = name;
+        this.eag = eag;
+    }
+
+@Override
+public int compareTo (Object obj){
+        Person entr = (Person) obj;
+        int result = name.compareTo(entr.name);
+        if (result!=0) return result;
+    if (eag<entr.eag) return -1;
+    else if (eag>entr.eag) return 1;
+    else return 0;
+}
+@Override
+public String toString(){
+        return String.format(TEMPLATE,name,eag);
+}
+/*    String getName(){
+        return name;
+    }
+    int getEag(){
+        return eag;
+    }
+}
+
+class PersonNameComparator implements Comparator<Person>{
+    @Override
+    public int compare(Person o1, Person o2) {
+        if (o1.getName().charAt(1)<o2.getName().charAt(1)) return 1;
+        else if (o1.getName().charAt(1)>o2.getName().charAt(1)) return -1;
+        else return 0;
+        return o1.getName().compareTo(o2.getName());
+    }
+}
+class PersonAgeComparator implements Comparator<Person>{
+    @Override
+    public int compare(Person o1, Person o2) {
+        if (o1.getEag()<o2.getEag()) return 1;
+        else if (o1.getEag()>o2.getEag()) return -1;
+        else return 0;
     }
 }
 
@@ -19,18 +61,28 @@ class Comparator{
         }
         return 0;
     }
+}*/
 }
 
 
 public class PersonComparator {
-    static Person[] persons = new Person[5];
+    //static Comparator<Person> pComparator = new PersonNameComparator().thenComparing(new PersonAgeComparator());
+    static TreeSet<Person> people = new TreeSet<>();
+    //static Person[] persons = new Person[5];
 
     public static void main(String[] args){
-        persons[0]=new Person("Толик", 30);
-        persons[4]=new Person("Толик", 25);
-        persons[2]=new Person("Николай", 35);
-        persons[1]=new Person("Денис", 25);
-        persons[3]=new Person("Игорь", 20);
+        people.add(new Person("Толик", 30));
+        people.add(new Person("Толик", 25));
+        people.add(new Person("Николай", 35));
+        people.add(new Person("Денис", 25));
+        people.add(new Person("Игорь", 20));
+        for (Person p : people) System.out.println(p.toString());
+        //Iterator ipeople = people.iterator();
+        /*person[0]=new Person("Толик", 30);
+        person[3]=new Person("Толик", 25);
+        person[2]=new Person("Николай", 35);
+        person[1]=new Person("Денис", 25);
+        person[4]=new Person("Игорь", 20);
         for (int i=0;i<persons.length;i++){
             for (int ii=1;ii<persons.length;ii++) {
                 if (i==ii)break;
@@ -49,6 +101,6 @@ public class PersonComparator {
                         break;
                 }
             }
-        }
+        }*/
     }
 }
