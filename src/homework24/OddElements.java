8180e3b35df7bb4be6d1dec15e4b5dcbf0e1cc8b
Toni <crazy_mane@mail.ru>
2020-05-21 00:22:54 +0300

    ДЗ 24 сделано.

---
+++ src/homework24/OddElements.java
@@ -0,0 +1,32 @@
+package homework24;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+class OddArray {
+    public Set<String> setArray(Set<String> set){
+        set.removeIf(s -> s.toString().length() % 2 == 0);
+        return set;
+    }
+}
+
+public class OddElements {
+    static Set<String> setStr = new HashSet<String>();
+    static OddArray steOddArray = new OddArray();
+    public static void main(String[] args){
+        setStr.add("foo");
+        setStr.add("buzz");
+        setStr.add("bar");
+        setStr.add("fork");
+        setStr.add("bort");
+        setStr.add("spoon");
+        setStr.add("!");
+        setStr.add("dude");
+        setStr = steOddArray.setArray(setStr);
+        System.out.println(setStr.toString());
+
+
+    }
+}
