f688b1062ab9c62014d44cbb42beb04826c8b3bf
Toni <crazy_mane@mail.ru>
2020-05-24 23:48:26 +0300

    ДЗ 25.1 IsUnique сделано.

---
+++ src/homework25/Unique.java
@@ -0,0 +1,29 @@
+package homework25;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+
+import java.util.Map;
+
+class IsUnique{
+    public boolean isUnique(Map<String, String> map){
+        boolean res = true;
+        for (String iter: map.values()) if (Collections.frequency(map.values(),iter)>1) res = false;
+        return res;
+    }
+}
+
+public class Unique {
+    static Map<String,String> prodMap = new HashMap<>();
+    static IsUnique isUnique = new IsUnique();
+    public static void main(String[] args){
+        prodMap.put("Вася","Иванов");
+        prodMap.put("Петр","Петров");
+        prodMap.put("Виктор","Сидоров");
+        prodMap.put("Сергей","Савельев");
+        prodMap.put("Вадим","Викторов");
+        prodMap.put("Иван","Петров");
+        System.out.println("Результат: " + isUnique.isUnique(prodMap));
+    }
+}
